2155f9aa8780fb086ee54cea35ac5f29cf6486b9
PierreAndreLouis <webdeveloper3030.com>
2024-09-09 11:05:41 -0400

    first commit

--- my_blog/src/components/web_pages/home/CTA12.jsx
+++ my_blog/src/components/web_pages/home/CTA12.jsx
@@ -1,29 +1,31 @@
-import { Button } from 'flowbite-react';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Button } from 'flowbite-react'
+import React from 'react'
 
 export default function CTA12() {
     return (
         <div
-            style={{ backgroundImage: 'url(https://img.freepik.com/premium-photo/team-young-african-people-office-table-with-laptop_219728-4569.jpg?w=900)', backgroundPosition: "right" }}
-            className='relative py-72 bg-fixed bg-cover bg-center'
-        >
-            <div className='absolute inset-0 bg-sky-100/90 dark:bg-gray-900/90'></div>
+            style={{ backgroundImage: 'url(https://img.freepik.com/premium-photo/close-up-group-different-colored-bottles-with-smoke-generative-ai_901275-40099.jpg?w=996)', backgroundPosition: "right" }}
+            className='object-right   relative py-72 bg-fixed bg-cover bg-center'>
 
-            <div className='flex flex-col justify-center items-center absolute inset-0 z-10 px-4'>
-                <h1 className='text-black dark:text-gray-100 text-3xl sm:text-4xl text-center font-bold max-w-4xl leading-9'>
-                    Envie d'un accompagnement <span className='text-sky-400'>personnalisé pour atteindre vos objectifs</span> ?
-                </h1>
-                <p className='text-center text-gray-900 dark:text-gray-100 mt-6 text-md leading-6 max-w-3xl'>
-                    Nous proposons des solutions sur mesure adaptées à vos besoins spécifiques. Avec un soutien constant et des stratégies efficaces, nous vous aidons à transformer vos objectifs en réussites concrètes.
+            <div className='absolute inset-0 z-[2]' style={{ backgroundColor: 'rgba(0, 0, 0, 0.80)' }}></div>
+
+
+            <div className='flex flex-col px-10 
+           justify-center items-center absolute inset-0 z-[3]'>
+                <p className='text-center text-gray-300 mt-10 text-md leading-6 lg:text-md max-w-3xl'>
+                    Nous nettoyons, vous vous détendez                </p>
+                <h1 className='text-white font-cursive text-2xl  sm:text-3xl text-center font-bold lg:text-4xl max-w-4xl leading-9'>
+                    Etes vous a la recherche de
+                    <span className='text-yellow-400'> service de nettoyage  </span> pour une activite?</h1>
+                <p className='text-center text-gray-300 mt-10 text-md leading-6 lg:text-md max-w-3xl'>
+                    nous offrons des solutions sur mesure qui répondent à vos besoins spécifiques, avec un soutien constant pour garantir votre succès. Nous faisons en sorte que chaque stratégie soit efficace et adaptée à vos objectifs.
                 </p>
-                <Link
-                    to='/reservation'
-                    className='border border-sky-500 text-sky-500 rounded-md mt-8 p-3 px-8 font-bold text-lg transition-colors hover:bg-sky-500 hover:text-white'
-                >
-                    Prenez un Rendez-vous
-                </Link>
+                <button
+                    className='border border-yellow-400 text-yellow-400 rounded-md mt-12 p-3 px-8 font-bold text-md'>
+                    Contactez nous
+                </button>
             </div>
+
         </div>
-    );
+    )
 }
